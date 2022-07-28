29510aa1e36b6acfea1b3ef706f8be0ba79b4cc5
alonso-estevam <alonso_estevam@outlook.com>
2022-07-28 17:47:03 -0300

    style: remove commented code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,3 @@
-// import { openDb } from './configDB.js';
-// import { createTable, insertCharacter, updateCharacter, selectAllCharacters, selectCharacter, deleteCharacter } from './controller/character-controller.js';
-
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,49 +7,6 @@ app.use(express.json());
 import router from './routes.js';
 
 app.use(router);
-
-// openDb() was removed
-//createTable();
-
-// app.get('/', (req, res) => {
-//     res.send("Hello world!")
-// });
-
-// app.get('/characters', async (req, res) => {
-//     let characters = await selectAllCharacters();
-//     res.json(characters);
-// })
-
-// app.get('/character', async (req, res) => {
-//     let character = await selectCharacter(req.body.id);
-//     res.json(character);
-// })
-
-// app.post('/characters', (req, res) => {
-//     insertCharacter(req.body);
-//     res.json({
-//         "statusCode": 201
-//     })
-// });
-
-// app.put('/characters', (req, res) => {
-//     if(req.body && !req.body.id) {
-//         res.json({
-//             "statusCode": "400",
-//             "message": "you need to inform some id"
-//         })
-//     } else {
-//         updateCharacter(req.body)
-//         res.json({
-//             "message": "Character successfully updated!"
-//         })
-//     }
-// });
-
-// app.delete('/character', async (req, res) => {
-//     let character = await deleteCharacter(req.body.id);
-//     res.json(character);
-// })
 
 app.listen(PORT, () => {
     console.log(`Server is running in http://localhost:${PORT}`);
