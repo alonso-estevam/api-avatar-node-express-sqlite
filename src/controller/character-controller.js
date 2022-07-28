cbdb96c990c7414f82528dd504b78677e344d896
alonso-estevam <alonso_estevam@outlook.com>
2022-07-28 14:47:42 -0300

    feat: create insertCharacter function

--- src/controller/character-controller.js
+++ src/controller/character-controller.js
@@ -5,3 +5,9 @@ export async function createTable() {
         db.exec('CREATE TABLE IF NOT EXISTS Character (id INTEGER PRIMARY KEY, name TEXT, nation TEXT, bender INTEGER)')
     })
 }
+
+export async function insertCharacter(character) {
+    openDb().then(db => {
+        db.run('INSERT INTO Character (name, nation, bender) VALUES (?,?,?)', [character.name, character.nation, character.bender])
+    });
+}
