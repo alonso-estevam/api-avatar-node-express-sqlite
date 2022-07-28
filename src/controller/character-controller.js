45060a2e56ce7e6313de11d962e057e9a9065dec
alonso-estevam <alonso_estevam@outlook.com>
2022-07-28 17:48:54 -0300

    style: adjust functions order

--- src/controller/character-controller.js
+++ src/controller/character-controller.js
@@ -6,18 +6,6 @@ export async function createTable() {
     })
 }
 
-export async function insertCharacter(character) {
-    openDb().then(db => {
-        db.run('INSERT INTO Character (name, nation, bender) VALUES (?,?,?)', [character.name, character.nation, character.bender])
-    });
-}
-
-export async function updateCharacter(character) {
-    openDb().then(db => {
-        db.run('UPDATE Character SET name=?, nation=?, bender=? WHERE id=?', [character.name, character.nation, character.bender, character.id])
-    });
-}
-
 export async function selectAllCharacters(req, res) {
     openDb().then(db => {
         db.all('SELECT * FROM Character')
@@ -33,9 +21,35 @@ export async function selectCharacter(req, res) {
     });
 }
 
-export async function deleteCharacter(id) {
-    return openDb().then(db => {
-        return db.get('DELETE FROM Character WHERE id=?', [id])
+export async function insertCharacter(req, res) {
+    let character = req.body;
+    openDb().then(db => {
+        db.run('INSERT INTO Character (name, nation, bender) VALUES (?,?,?)', [character.name, character.nation, character.bender])
+    });
+    res.json({
+        "statusCode": 201
+    })
+}
+
+export async function updateCharacter(req, res) {
+    let character = req.body;
+    openDb().then(db => {
+        db.run('UPDATE Character SET name=?, nation=?, bender=? WHERE id=?', [character.name, character.nation, character.bender, character.id])
+    });
+    res.json({
+        "statusCode": 201,
+        "message": "Character successfully updated!"
+    })
+}
+
+export async function deleteCharacter(req, res) {
+    let id = req.body.id;
+    openDb().then(db => {
+        db.get('DELETE FROM Character WHERE id=?', [id])
         .then(res => res)
     });
+    res.json({
+        "statusCode": 200,
+        "message": "Character successfully deleted!"
+    })
 }
