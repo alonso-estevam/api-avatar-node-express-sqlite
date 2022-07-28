181762da7e7db46ac872cf8d385ffa76298621f1
alonso-estevam <alonso_estevam@outlook.com>
2022-07-28 17:48:00 -0300

    feat: set up initial route

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,18 @@ import { Router } from 'express';
 import { createTable, insertCharacter, updateCharacter, selectAllCharacters, selectCharacter, deleteCharacter } from './controller/character-controller.js';
 
 const router = Router();
-router.get('/characters', selectAllCharacters)
-router.get('/character', selectCharacter)
+
+router.get('/', (req, res) => {
+    res.json({
+        "statusCode": 200,
+        "message": "API running"
+    })
+})
+
+router.get('/characters', selectAllCharacters);
+router.get('/character', selectCharacter);
+router.post('/characters', insertCharacter);
+router.put('/character', updateCharacter);
+router.delete('/character', deleteCharacter);
 
 export default router;
