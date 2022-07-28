35e79838ceb54d64f1d860a14c01e815fa693d71
alonso-estevam <alonso_estevam@outlook.com>
2022-07-28 13:00:26 -0300

    feat: add post method

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello world!")
+});
+
+app.post('/characters', (req, res) => {
+    console.log(req.body);
+    res.json({
+        "statusCode": 200
+    })
 });
 
 app.listen(PORT, () => {
